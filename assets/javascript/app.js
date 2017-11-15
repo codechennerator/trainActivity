e8ee51d2eb3b55313030c648f929c3b0466a7017
Nathan Chen <nathanchen@Nathans-MacBook-Air.local>
2017-11-14 19:35:29 -0800

    added live updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,45 +10,58 @@
 
 var defaultDatabase = firebase.database();
 
-
+//interval listener
+var timerId;
 //Event happens once
 var childListener = false;
 //Database clarity
 var ref = defaultDatabase.ref();
 var filter = ref.orderByChild('dateAdded').limitToLast(1);
 
-
-function createTable(info){
-	console.log(info);
+/*This function returns an array of the minutesToNextTrain and the nextTrain.*/
+function calculateTime(info){
 	var trainFreq = parseInt(info.frequency);
 	var now = moment();
-	var firstTime = moment(info.firstTrain, "HH:mm").subtract(1, "days");
+	var firstTime = moment(info.firstTrain, "HH:mm");
 
 	var differenceInTime = now.diff(moment(firstTime), "minutes");
-	console.log("differenceInTime var: " + differenceInTime);
-	var remainder = differenceInTime % trainFreq;
-	console.log("remainder var: " + remainder);
-	var minutesToNextTrain = trainFreq - remainder;
-	
+	//edge case
+	if (differenceInTime < 0){
+		firstTime = moment(info.firstTrain, "HH:mm").subtract(1, "days");
+		differenceInTime = now.diff(moment(firstTime), "minutes");
+	}
 
+	var remainder = differenceInTime % trainFreq;
+	var minutesToNextTrain = trainFreq - remainder;
 	var nextTrain = now.add(minutesToNextTrain, "minutes").format('hh:mm A');
 	
-	$('#trainTable').append(
-		'<tr>' +
+	return [nextTrain, minutesToNextTrain];
+
+}
+function createTable(info){
+	console.log(info);
+
+	var calculatedInfo = calculateTime(info);
+
+	var $trainRowDiv = $('<tr>').attr('id', info.dateAdded);
+	$trainRowDiv.append(
 		'<td>' + info.trainName + '</td>' +
 		'<td>' + info.destination + '</td>' +
 		'<td>' + info.frequency + '</td>' +
-		'<td>' + nextTrain + '</td>' +
-		'<td>' + minutesToNextTrain + '</td>' +
-		'</tr>'
+		'<td class = "nextArrivalData">' + calculatedInfo[0] + '</td>' +
+		'<td class = "minutesAwayData">' + calculatedInfo[1] + '</td>'
 	);
+
+	$('#trainTable').append($trainRowDiv);
 }
+
 
 defaultDatabase.ref().once("value", function(snapshot) {
 	snapshot.forEach(function(child){
 		createTable(child.val());
 	});
 });
+
 
 filter.on('child_added',function(snapshot){
 	if(childListener){
@@ -65,7 +78,7 @@ $('#run-submit').on('click',function(event){
 		if(!childListener) childListener = true;
 		var trainIn = $('#trainName').val().trim();
 		var destinationIn = $('#destination').val().trim();
-		var firstTimeIn = $('#firstTrainTime').val().trim();
+		var firstTimeIn = $('#firstTrainTime').val().trim();	
 		var frequencyIn = $('#frequency').val().trim();
 
 
@@ -85,3 +98,18 @@ $('#run-submit').on('click',function(event){
 		$('#frequency').val('');
 	}
 });
+
+/*-------------------Timer Functionality-------------------------*/
+
+function updateTable(){
+	defaultDatabase.ref().once("value", function(snapshot) {
+		snapshot.forEach(function(child){
+			var rowIdChildren = document.getElementById(child.val().dateAdded).childNodes;
+			var newInfo = calculateTime(child.val());
+			
+			rowIdChildren[3].innerHTML = newInfo[0];
+			rowIdChildren[4].innerHTML = newInfo[1];
+		});
+	});
+}
+timerId = setInterval(updateTable, 60000);
